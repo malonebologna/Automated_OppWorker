2f4a88399f29c522f9cb27eb215f2ef9521e037f
malonebologna <32462823+malonebologna@users.noreply.github.com>
2018-01-26 10:58:51 -0500

    Intial Commit Created Folder Structure and 4 sub programs
    Structure is as follows:
    Main Program: Automated_OppWorker_Project
    
    1: Machform_Entry_Downloader
    (Will download entries from Machform at certain time intervals automatically)
    
    2: OppEntry_Html_Creator
    (Will create one email Html string with the values from the Machform entries included)
    
    3: Opp_Email_Manager
    (Will send and log all emails)
    
    4: Import_File_Handler
    (Will take all data and create one .xls file tailored for HRTMS Importer)
    
    1-26-18

---
+++ Automated_OppWorker_Project/OppEntry_Html_Creator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppEntry_Html_Creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
